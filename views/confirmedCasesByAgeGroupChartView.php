995774c1195b40ec3ec3876fd52f449bcae70a56
jncorp <jncorpuz@icloud.com>
2020-04-30 00:01:45 +0800

    Added Views for Pie Charts

---
+++ views/confirmedCasesByAgeGroupChartView.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<div class="card card-danger2">
+    <div class="card-header">
+        <h3 class="card-title" style="color: white;">CASES BY AGE GROUP</h3>
+    </div>
+    <div class="card-body">
+        <div class="chart">
+            <canvas id="stackedBarChart"
+                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+        </div>
+    </div>
+</div>

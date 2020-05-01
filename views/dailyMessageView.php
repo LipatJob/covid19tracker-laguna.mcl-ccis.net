e330eb0669e287e21ecd0f9e9ff4807eeae70861
LipatJob <35028794+LipatJob@users.noreply.github.com>
2020-05-01 21:07:51 +0800

    +added message of hope pictures;

--- views/dailyMessageView.php
+++ views/dailyMessageView.php
@@ -1,45 +1,63 @@
 <?php
-    $images = [ "imgs/imagesoftheday/0.jpg",
-                "imgs/imagesoftheday/1.jpg",
-                "imgs/imagesoftheday/2.jpg",
-                "imgs/imagesoftheday/3.jpg",
-                "imgs/imagesoftheday/4.jpg"];
+    $images = [ "imgs/imagesoftheday/MessagesofHope.001.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.002.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.003.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.004.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.005.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.006.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.006.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.007.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.008.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.009.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.010.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.011.jpeg",
+                "imgs/imagesoftheday/MessagesofHope.012.jpeg"
+                ];
 
-    $currentDate = intval(date("d"));
+    $currentDate = intval(date("i"));
     $imageIndex = $currentDate % 5;
     $imageLocation = $images[$imageIndex];
 ?>
 
 <style>
 @media (min-width: 992px) {
-  .modal-dialog {
-    max-width: 40%;
-  }
+    .modal-dialog {
+        max-width: 45%;
+    }
 }
 </style>
 
+<link href="https://fonts.googleapis.com/css2?family=Neucha&display=swap" rel="stylesheet">
 
 <!-- Modal -->
-<div class="modal fade" id="videomodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="videomodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
     <div class="modal-dialog modal-md modal-dialog-centered" role="video">
-        <div class="modal-content" style="background-color: #095779; color: white;">
-            <div class="modal-header" style = "border-bottom:none;">
+        <div class="modal-content" style="background-color: #00000; color: black;">
+            <div class="modal-header" style="border-bottom:none;">
                 <div class="parent-container-horizontal">
-                    <h5 class="modal-title" id="exampleModalLongTitle" style="font-size: 1.2em; font-weight: 400; margin-left: 10px; text-align:center;">
+                    <p class="mx-auto" style="font-weight:bold; font-size:1.3em">
+                        A Message of Hope
+                    </p>
+                    <p id="exampleModalLongTitle" style="font-size: 1.2em; font-weight: 400; 
+                    margin-left: 10px; text-align:center;">
                         We send love and prayers to Covid-19 patients, their families, and the courageous frontliners
-                        through our drawings and messages of hope. ❤
-                    </div>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true" style = "color:white;">&times;</span>
-                    </button>
+                        through drawings and messages of hope. ❤
+                    </p>
                 </div>
-                <div class="modal-body">
-                    <div class="wrapper">
+            </div>
+            <div class="modal-body">
+                <div class="wrapper">
                     <img src="<?php echo $imageLocation?>" alt="" srcset="" width="100%" height="auto"
-                        style="border:none;overflow:hidden; margin: 15px; margin-top:0px" scrolling="no" frameborder="0" allowTransparency="true"
-                        allowFullScreen="true" class = "shadow-sm mx-auto">
-                    </div>
+                        style="border:none;overflow:hidden; margin: 15px; margin-top:0px" scrolling="no" frameborder="0"
+                        allowTransparency="true" allowFullScreen="true" class=" mx-auto">
+                </div>
+                <div class = "row">
+                    <button type="button" class="btn btn-outline-secondary mx-auto" data-dismiss="modal" aria-label="Close"
+                        style="">
+                        <span aria-hidden="true" style="color:black;">Close</span>
+                    </button>
                 </div>
             </div>
         </div>
-        
+    </div>

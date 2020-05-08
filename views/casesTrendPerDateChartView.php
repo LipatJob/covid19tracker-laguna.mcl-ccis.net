304d0cc32f33bd29daa4ae3f7112499535048b75
LipatJob <35028794+LipatJob@users.noreply.github.com>
2020-05-08 17:54:25 +0800

    -removed toggle for moving average;

--- views/casesTrendPerDateChartView.php
+++ views/casesTrendPerDateChartView.php
@@ -6,20 +6,13 @@ $data = getCachedCurrentTrend($_GET["location"]);
 
 <style>
 
-@media screen and (max-width: 900px) {
-    #navButtonContainer{
-        margin-right: 100px
-    }
-}
+
     
 </style>
 
 <div class="card card-danger2">
     <div class="card-header">
         <div class="pt-1">
-            <div id = "navButtonContainer" class="btn-group btn-group-toggle float-lg-right float-sm-left"  data-toggle="buttons">
-                <button type="button" id='toggleTrendCasesChart' class="btn btn-sm btn-primary" value="graph">SHOW MOVING AVERAGES</button>
-            </div>
             <h3 class="card-title mt-1 ml-1 float-lg-left float-sm-right" style="color: white;">NEW CONFIRMED CASES</h3>
         </div>
 
@@ -41,7 +34,11 @@ $data = getCachedCurrentTrend($_GET["location"]);
 <script>
 $(function() {
     var debugMode = true;
-    var movingAverageData = [{
+    var areaChartData = {
+
+        labels: <?php echo json_encode($data["dates"]) ?> ,
+        datasets: [
+            {
             label: 'MOVING AVERAGE',
             type: 'line',
             borderColor: '#79a5b8',
@@ -50,25 +47,7 @@ $(function() {
             pointHighlightStroke: '#79a5b8',
             lineTension: 0,
             data: <?php echo json_encode($data["MovingAverage"]) ?>
-        }
-        ];
-    var rawData = [{
-            label: 'NEW CASES',
-            type: 'bar',
-            backgroundColor: '#080E85',
-            borderColor: '#080E85',
-            pointRadius: true,
-            pointColor: '#3b8bba',
-            pointStrokeColor: '#ffcc00',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: '#ffcc00',
-            data: <?php echo json_encode($data["NewCases"]) ?>
-        }];
-
-    var areaChartData = {
-
-        labels: <?php echo json_encode($data["dates"]) ?> ,
-        datasets: [
+        },
         {
             label: 'NEW CASES',
             type: 'bar',
@@ -81,6 +60,7 @@ $(function() {
             pointHighlightStroke: '#ffcc00',
             data: <?php echo json_encode($data["NewCases"]) ?>
         }
+        
         ]
     }
 
@@ -153,24 +133,6 @@ $(function() {
         options: lineChartOptions
     })
 
-    var isMovingAverage = false;
-    $("#toggleTrendCasesChart").on("click", function() {
-        if(isMovingAverage){
-            $("#toggleTrendCasesChart").text("SHOW MOVING AVERAGE");
-            isMovingAverage = false;
-            lineChart.data.datasets = rawData;
-            
-        }else{
-            $("#toggleTrendCasesChart").text("SHOW DAILY NEW CASES");
-            isMovingAverage = true;
-            lineChart.data.datasets = movingAverageData;
-
-        }
-        lineChart.update();
-        if(debugMode){
-            jQuery('#checkData').html(dataToTable(lineChart.data));
-        }
-   });
 
 var dataToTable = function (dataset) {
     var html = '<table>';

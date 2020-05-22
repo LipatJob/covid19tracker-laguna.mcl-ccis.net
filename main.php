e5c9bf4eafccd01278fa0d3a727a4c37cab37884
cedricflores <63637386+cedricflores@users.noreply.github.com>
2020-05-22 21:29:03 +0800

    switched table to upper portion

--- main.php
+++ main.php
@@ -63,6 +63,63 @@
         </div>
     </div>
 
+            <div class="container-fluid">
+                <div id='table' class="row">
+                    <div class="container-fluid" id="tableContainer">
+                        <div id='table' class="row">
+                            <div class="container-fluid" id="divCount">
+                                <div class="card card-outline" style="padding: 0px;">
+                                    <div class="card-body" style="overflow: scroll;">
+                                        <div class="table-responsive">
+                                            <table id="overviewTable" class="table table-hover table-striped table-bordered datatable table-light rounded-5">
+                                                <thead id="header" class="" style="color:white; background-color: #354664; border-radius:5px">
+                                                    <tr>
+                                                        <th class=""></th>
+                                                        <th></th>
+                                                        <th></th>
+                                                        <th></th>
+                                                        <th></th>
+                                                        <th></th>
+                                                        <th></th>
+                                                        <th></th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody class="">
+                                                    <tr>
+                                                        <td class=""></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                    </tr>
+                                                </tbody>
+                                                <tfoot id="footer">
+                                                    <tr>
+                                                        <td class=""></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                        <td></td>
+                                                    </tr>
+
+                                                </tfoot>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
     <!-- Graphs -->
     <div class="container-fluid">
         <div class="row">
@@ -122,62 +179,7 @@
             <div id='summaryPerMunicipalityCityChart'>
 
             </div>
-            <div class="container-fluid">
-                <div id='table' class="row">
-                    <div class="container-fluid" id="tableContainer">
-                        <div id='table' class="row">
-                            <div class="container-fluid" id="divCount">
-                                <div class="card card-outline" style="padding: 0px;">
-                                    <div class="card-body" style="overflow: scroll;">
-                                        <div class="table-responsive">
-                                            <table id="overviewTable" class="table table-hover table-striped table-bordered datatable table-light rounded-5">
-                                                <thead id="header" class="" style="color:white; background-color: #354664; border-radius:5px">
-                                                    <tr>
-                                                        <th class=""></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody class="">
-                                                    <tr>
-                                                        <td class=""></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                    </tr>
-                                                </tbody>
-                                                <tfoot id="footer">
-                                                    <tr>
-                                                        <td class=""></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                    </tr>
 
-                                                </tfoot>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
 
 
 
